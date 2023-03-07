065132d1ee94116e61a0360a10b6726c14839b48
darkeny <darkenpaulo00@gmail.com>
2023-03-07 21:36:19 +0200

    implements home page

--- ui/evolution/src/pages/home/index.tsx
+++ ui/evolution/src/pages/home/index.tsx
@@ -1,1 +1,14 @@
 import { } from 'styled-components'
+import { Container, Content_Center, H1 } from './styles';
+const Home = () => {
+    return (
+        <>
+            <Container>
+                <Content_Center>
+                    <H1>Sign in</H1>
+                </Content_Center>
+            </Container>
+        </>
+    )
+}
+export default Home;
