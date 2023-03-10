5ed91081d8b6f73eef4a2232beb6a5a8f7f4f87d
darkeny <darkenpaulo00@gmail.com>
2023-03-10 19:17:42 +0200

    creating style Container with styled components

--- ui/evolution/src/pages/sign-in/index.tsx
+++ ui/evolution/src/pages/sign-in/index.tsx
@@ -1,7 +1,8 @@
+import styled from "styled-components";
 const SignIn = () => {
     return (
         <>
-            <h1>Sign-in page</h1>
+            
         </>
     )
 }
---
+++ ui/evolution/src/pages/sign-in/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    padding-left: 15px;
+    padding-right: 15px;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 1140px;
+
+`
