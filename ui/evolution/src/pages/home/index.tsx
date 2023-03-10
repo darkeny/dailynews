e1887cfaad04819f6402ce3f0a2b3c5bedb069c6
darkeny <darkenpaulo00@gmail.com>
2023-03-10 19:08:18 +0200

    fix  mistakes in the home page

--- ui/evolution/src/pages/home/index.tsx
+++ ui/evolution/src/pages/home/index.tsx
@@ -1,22 +1,9 @@
 import { } from 'styled-components'
-import { Button, Container, Content_Center, Footer, H1, Paragraph } from './styles';
+
 const Home = () => {
     return (
         <>
-            <Container>
-                <Button>
-                    Sign-up
-                </Button>
-                <Content_Center>
-                    <H1>Sign in</H1><br />
-                    
-                </Content_Center>
-                <Footer>
-                    <Paragraph>
-                        @Copyrigth 22-23
-                    </Paragraph>
-                </Footer>
-            </Container>
+
         </>
     )
 }
