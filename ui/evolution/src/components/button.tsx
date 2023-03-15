40b6f634559472adfc36b3c86629107349762b30
darkeny <darkenpaulo00@gmail.com>
2023-03-15 23:52:18 +0200

    adding styles to the button

--- ui/evolution/src/components/button.tsx
+++ ui/evolution/src/components/button.tsx
@@ -1,17 +1,18 @@
 import { FC } from "react";
+import { Button as FCButton } from './styles'
 
 type buttonProps = {
     label: string;
-    disabled: boolean;
+    disabled?: boolean;
     onClick: () => void;
 }
 
 const Button: FC<buttonProps> = ({ label, disabled, onClick }) => {
     return (
         <>
-            <button onClick={onClick} disabled={disabled}>
+            <FCButton onClick={onClick}>
                 {label}
-            </button>
+            </FCButton>
         </>
     )
 
--- ui/evolution/src/pages/sign-in/index.tsx
+++ ui/evolution/src/pages/sign-in/index.tsx
@@ -1,14 +1,15 @@
+import Button from "../../components/button";
 import { Container, Content_Center, Footer, Form, H1, Input, Paragraph } from "./styles";
 const SignIn = () => {
+    function Headler() {
+        console.log('Clicou')
+    }
     return (
         <>
+            <Button label={'Sign-in'} onClick={Headler} disabled />
             <Container>
                 <Content_Center>
                     <H1>Sign-in</H1>
-                    <Form action={'#'}>
-                        <Input type={'text'} name={'username'} required />
-                        <Input type={'password'} name={'password'} required />
-                    </Form>
                 </Content_Center>
                 <Footer>
                     <Paragraph>@Copyright 2022 - 2023</Paragraph>
